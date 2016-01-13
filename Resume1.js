58b836fa7014ba0338f4e11f913a7767a68194e3
surya <thotasuryaprakash246@gmail.com>
2016-01-13 17:06:05 +0530

    new Resume

---
+++ Resume1.js
@@ -0,0 +1,6 @@
+function count(){
+  document.getElementById("des").innerHTML = 	"In this project a new cascaded multilevel inverter using developed H bridge is proposed and developed.The proposed topology requires a lesser number of dc   voltage sources and power switches and lower blocking voltage on switches,which results in decreased complexity and total cost of the inverter."
+} 
+function count1(){
+document.getElementById("des").innerHTML=" ";
+}
